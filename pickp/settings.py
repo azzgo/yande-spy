64c1f3d099570e9e84a02d3ea8999c6cc65a018b
loomla <wittywys@gmail.com>
2014-06-11 15:47:50 +0800

    init

---
+++ pickp/settings.py
@@ -0,0 +1,15 @@
+# Scrapy settings for pickp project
+#
+# For simplicity, this file contains only the most important settings by
+# default. All the other settings are documented here:
+#
+#     http://doc.scrapy.org/en/latest/topics/settings.html
+#
+
+BOT_NAME = 'pickp'
+
+SPIDER_MODULES = ['pickp.spiders']
+NEWSPIDER_MODULE = 'pickp.spiders'
+
+# Crawl responsibly by identifying yourself (and your website) on the user-agent
+#USER_AGENT = 'pickp (+http://www.yourdomain.com)'
